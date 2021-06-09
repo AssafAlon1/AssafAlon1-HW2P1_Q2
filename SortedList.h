bb086a27756c51fdda7b2d60c2baa7eddeb9b924
AssafAlon1 <alon.assaf@campus.technion.ac.il>
2021-06-09 17:14:47 +0300

    First

---
+++ SortedList.h
@@ -0,0 +1,135 @@
+#ifndef _SORTED_LIST_H
+#define _SORTED_LIST_H
+
+// Assuming class T has:
+// Copy constructor, Destructor, operator<,
+#define T int
+
+class SortedList
+{
+public:
+    SortedList();
+    //~SortedList();
+    //SortedList(const SortedList& sorted_list);
+    //operator=
+    void insert(T element);
+    //remove
+    int length();
+    //filter
+    //apply
+    //begin
+    //end
+
+    
+
+private:
+    class Node;
+    
+    int size;
+    Node *first;
+    //Node *last;
+
+    Node* findPlacement(T element);
+};
+
+
+class SortedList::Node
+{
+public:
+    Node(T element);
+    Node(const Node& node) = default;
+    Node& operator=(const Node& node);
+    ~Node() = default;
+
+    void setNext(Node* node);
+    Node* getNext();
+    T& getData();
+
+private:
+    T data;
+    SortedList::Node *next;
+};
+
+
+//=========================================
+//========== Node implementation ==========
+//=========================================
+SortedList::Node::Node(T element)
+{
+    data = T(element);
+    next = nullptr;
+}
+
+void SortedList::Node::setNext(SortedList::Node* node)
+{
+    this->next = node;
+}
+
+SortedList::Node* SortedList::Node::getNext()
+{
+    return this->next;
+}
+
+T& SortedList::Node::getData()
+{
+    return this->data;
+}
+
+
+
+
+//=========================================
+//======= SortedList implementation =======
+//=========================================
+
+// Returns nullptr if the list is currently empty OR the placement is the first node
+// Otherwise returns the node that should be placed before a new node containing element in the list
+SortedList::Node* SortedList::findPlacement(T element)
+{
+    if (size == 0 || element < first->getData())
+    {
+        return nullptr;
+    }
+
+    return nullptr; // <---- implement here
+}
+
+
+SortedList::SortedList()
+{
+    size   = 0;
+    first  = nullptr;
+    //last   = nullptr;
+}
+
+void SortedList::insert(T element)
+{
+    Node new_node = Node(element);
+
+    // First element
+    if (size == 0)
+    {
+        first = &new_node;
+        size++;
+        return;
+    }
+
+    if (element < first->getData())
+    {
+        new_node.setNext(first);
+        first = &new_node;
+    }
+
+    // General case - new node should be placed somewhere in the middle
+    Node* prev_node = findPlacement(element);
+    new_node.setNext(prev_node->getNext());
+    prev_node->setNext(&new_node);
+    
+}
+
+int SortedList::length()
+{
+    return this->size;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include "SortedList.h"
+#include <assert.h>
+
+// void nodeTests()
+// {
+//     Node n1 = Node(3);
+//     Node n2 = Node(6);
+//     Node n3 = Node(4);
+//     Node n4 = Node(13);
+//     n1.setNext(&n2);
+//     n2.setNext(&n3);
+//     n3.setNext(&n4);
+
+//     assert(n1.getData() == 3);
+//     assert(n2.getData() == 6);
+//     assert(n3.getData() == 4);
+//     assert(n4.getData() == 13);
+
+//     assert(n1.getNext()->getData() == 6);
+//     assert(n1.getNext()->getData() == 4);
+//     assert(n1.getNext()->getData() == 13);
+
+//     n1.setNext(&n4);
+//     n4.setNext(&n2);
+//     n3.setNext(&n1);
+
+//     assert(n1.getNext()->getData() == 13);
+//     assert(n1.getNext()->getNext()->getData() == 6);
+//     assert(n2.getNext()->getNext()->getNext()->getData() == 4);
+//     assert(n1.getNext()->getNext()->getNext()->getNext()->getData() == 6);
+// }
+
+int main ()
+{
+    SortedList x();
+    
+    return 0;
+}
